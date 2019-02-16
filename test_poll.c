412d6dd45b7344d93e366b7c6bbec1ac761e1076
fenshitianyue <abzanda@163.com>
2019-02-16 23:57:56 +0800

    had operation

--- test_poll.c
+++ test_poll.c
@@ -4,6 +4,24 @@
 #include <poll.h>
 
 int main() {
-  
+  struct pollfd poll_fd; 
+  poll_fd.fd = 0;
+  poll_fd.events = POLLIN;
+  while(1){
+    int ret = poll(&poll_fd, 1, 1000);
+    if(ret < 0){
+      perror("poll");
+      continue;
+    }
+    if(0 == ret){
+      printf("poll timeout\n");
+      continue;
+    }
+    if(poll_fd.revents == POLLIN){
+      char buf[1024] = {0};
+      read(0, buf, sizeof(buf) - 1);
+      printf("stdin:%s\n", buf);
+    }
+  }
   return 0;
 }
