7aab0bae29636aba41244c98b85d53a4c07a0110
fenshitianyue <abzanda@163.com>
2019-03-15 19:29:18 +0800

    test splice function

---
+++ splice_echo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <assert.h>
+
+int main() {
+  
+  return 0;
+}
