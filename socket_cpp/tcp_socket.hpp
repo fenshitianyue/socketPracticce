bd636c2cd56f162de8075b25d7f6d6baae96ecf0
fenshitianyue <abzanda@163.com>
2019-06-21 17:15:08 +0800

    update code, complete framework code

--- socket_cpp/tcp_socket.hpp
+++ socket_cpp/tcp_socket.hpp
@@ -16,36 +16,85 @@ public:
   TcpSocket(int fd) : _fd(fd) {}
 
   bool Socket(){
-
-  }
-
-  bool Close() const {
+    _fd = socket(AF_INET, SOCK_STREAM, 0); 
+    if(_fd < 0){
+      perror("socket");
+      return false;
+    }
     return true;
   }
 
-  bool Bind() const {
+  bool Close() const {
+    close(_fd);
+    return true;
+  }
+
+  bool Bind(const std::string& ip, uint16_t port) const {
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(ip.data());
+    if(bind(_fd, (sockaddr*)&addr, sizeof(addr)) < 0){
+      perror("bind");
+      return false;
+    }
     return true;
   }
 
   bool Listen(int num) const {
-    
+    if(listen(_fd, num) < 0){
+      perror("listen");
+      return false;
+    } 
     return 0;
   }
 
-  bool Accept() const {
-
+  bool Accept(TcpSocket& peer, std::string& ip, uint16_t& port) const {
+    sockaddr_in peer_addr;
+    socklen_t len = sizeof(peer_addr);
+    int new_sock;
+    if((new_sock = accept(_fd, (sockaddr*)&peer_addr, &len)) < 0){
+      perror("accept");
+      return false;
+    }
+    peer._fd = new_sock;
+    ip = inet_ntoa(peer_addr.sin_addr);
+    port = ntohs(peer_addr.sin_port);
+    return true;
   }
 
-  bool Recv() const {
-
+  bool Recv(std::string& buf) const {
+    buf.clear();
+    char tmp[4096] = {0};
+    ssize_t read_size = recv(_fd, tmp, sizeof(tmp), 0);
+    if(read_size < 0){ //recv 失败
+      perror("recv");
+      return false;
+    }else if(0 == read_size){  //未读取到数据
+      return false;
+    }
+    buf.assign(tmp, read_size); //填充读取缓冲区数据
+    return true; 
   }
   
-  bool Send() const {
-
+  bool Send(const std::string& buf) const {
+    if(send(_fd, buf.data(), buf.size(), 0) < 0){
+      perror("send");
+      return false;
+    }
+    return true;
   }
 
-  bool Connect() const {
-
+  bool Connect(const std::string& ip, uint16_t port) const {
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(ip.data());
+    if(connect(_fd, (sockaddr*)&addr, sizeof(addr)) < 0){
+      perror("connect");
+      return false;
+    }
+    return true;
   }
 
   int GetFd() const {
